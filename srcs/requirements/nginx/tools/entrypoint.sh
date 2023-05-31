49ea173e81f85e797f857c40c7a37aa13bb41626
Houssam Abouiba <habouiba@e1r5p6.1337.ma>
2023-05-31 18:11:24 +0100

    adding tls

--- srcs/requirements/nginx/tools/entrypoint.sh
+++ srcs/requirements/nginx/tools/entrypoint.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+
+mkdir /etc/nginx/ssl
+
+chmod 755 /etc/nginx/ssl
+
+openssl genrsa -out /etc/nginx/ssl/habouiba.fr.key
+openssl req -x509 -config /etc/nginx/csr.conf -key /etc/nginx/ssl/habouiba.fr.key -out /etc/nginx/ssl/habouiba.fr.crt -days 365
+
 exec "$@"
