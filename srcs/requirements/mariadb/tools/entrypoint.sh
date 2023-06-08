84bdb079f67443527fa460ee3f331608c58ac628
Houssam Abouiba <habouiba@e2r6p4.1337.ma>
2023-06-08 16:10:56 +0100

    adding script for creating user and database

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,6 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 set -e
+mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql
+/etc/init.d/mariadb setup
+/etc/init.d/mariadb start
 
-# Switch to mariadbuser
-su -s /bin/sh -c "/usr/bin/mysqld" mariadbuser
+mysql -e "UPDATE mysql.user SET Password = PASSWORD('${MYSQL_ROOT_PASSWORD}') WHERE User = 'root'"
 
+mysql -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+
+# Kill the anonymous users
+mysql -e "DROP USER ''@'localhost'"
+
+# mysql -e "DROP USER ''@'$(hostname)'"
+
+mysql -e "DROP DATABASE test"
+
+mysql -e "FLUSH PRIVILEGES"
