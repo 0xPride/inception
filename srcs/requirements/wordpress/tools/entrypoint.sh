cd2cc29d9fe26a5b186960876b5efcf7de66ad08
0xpride <32068899+0xPride@users.noreply.github.com>
2023-06-16 18:17:26 +0100

    finish

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -6,17 +6,6 @@ WP=/var/www/html/habouiba.42.fr/
 
 chown -R www-data:www-data /var/www/html/
 
-# sed -i "s/database_name_here/${WP_DB}/" ${WP}wp-config.php
-# sed -i "s/username_here/${WP_DB_USR}/" ${WP}wp-config.php
-# sed -i "s/password_here/${WP_PWD}/" ${WP}wp-config.php
-# sed -i "s/localhost/${DB_HOSTNAME}/" ${WP}wp-config.php
-
-# mkdir /run/php/
-# chown www-data:www-data /run/php
-# exec "$@"
-
-# new method using wpcli
-
 if [ ! -d "${WP}" ]; then
   # installing wp-cli
   cd opt;
@@ -33,6 +22,7 @@ if [ ! -d "${WP}" ]; then
     --dbpass=${WP_PWD} \
     --dbhost=${DB_HOSTNAME}
   wp db create
+
 fi
 
 mkdir -p /run/php/
